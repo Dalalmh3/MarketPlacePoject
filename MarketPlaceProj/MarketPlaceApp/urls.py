252a6574475e0ba0ee25d42960e51126bcc19ccb
Dalal mh <dalalmansourh3@gmail.com>
2019-12-18 04:44:11 -0800

    c22

--- MarketPlaceProj/MarketPlaceApp/urls.py
+++ MarketPlaceProj/MarketPlaceApp/urls.py
@@ -3,4 +3,5 @@ from . import views
                     
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^upload_file$', views.upload_file),
 ]
--- MarketPlaceProj/MarketPlaceApp/views.py
+++ MarketPlaceProj/MarketPlaceApp/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render,redirect, HttpResponse
+from django.shortcuts import render,redirect
+from .models import Product
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage
+
 # Create your views here.
 
-# def index(request):
-#     return HttpResponse("this is the equivalent of @app.route('/')!")
-
-#     from django.shortcuts import render, redirect
 def index(request):
-    return render(request, "MarketPlaceApp/index.html" ) 
-    # if request.method == "POST":
-    #     val_from_field_one = request.POST["one"]
-    # 	val_from_field_two = request.POST["two"]
+    context= {
+        "all_products": Product.objects.all(),
+    }
+    return render(request, "MarketPlaceApp/index.html", context ) 
+
+
+def upload_file(request):
+
+    try:
+        if request.method == 'POST' : #and request.FILES['myfile']:
+            #The request.FILES will hold the uploaded file
+            myfile = request.FILES['myfile']
+            fs = FileSystemStorage() #store uploaded file
+            filename = fs.save(myfile.name, myfile) #save the name of uploaded file
+            fileURL = fs.url(filename) #convert the name of uploaded file to URL
+    
+            new_poduct = Product.objects.create( pName = request.POST['pname'] , description = request.POST['pDescription'], img_url = fileURL) #, img_url = fileURL)
+            messages.info(request, 'The product has been Created successfully!')
+            return redirect('/')
+                     
+    except Exception as e:
+        print (e)
+        messages.info(request, " Upload Image please") #error message if the user presses upload without uploading an image
+        return redirect("/")
