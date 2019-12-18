c951f037869df80bdead1f71319fc10652c4a355
Dalal mh <dalalmansourh3@gmail.com>
2019-12-18 11:01:38 -0800

    third

--- MarketPlaceProj/MarketPlaceApp/models.py
+++ MarketPlaceProj/MarketPlaceApp/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Product(models.Model):
    pName= models.CharField(max_length=45)
    description = models.TextField()
-   img_url = models.TextField()
+   img_url = models.ImageField(upload_to ='media/') #models.TextField()
    created_at = models.DateTimeField(auto_now_add=True)
    updated_at = models.DateTimeField(auto_now=True)
